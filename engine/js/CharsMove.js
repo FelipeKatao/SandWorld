56669f03d626f0e831d573dd5f716b9eb0efc599
Felipe Catão do Nascimento <34559081+FelipeKatao@users.noreply.github.com>
2020-06-14 14:38:51 -0300

    Update CharsMove.js

--- engine/js/CharsMove.js
+++ engine/js/CharsMove.js
@@ -2,7 +2,7 @@ var charTarget= document.getElementById("char")
 document.addEventListener("keydown",(event)=>{
     const keyname = event.key;
     if(event.keyCode==39){
-        charTarget.style.marginRight+=12
+        charTarget.style.marginRight+="1px";
         charTarget.style.backgroundColor="blue";
     }
 })
