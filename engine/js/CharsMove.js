5837629cf7446d8e87d02fa9c68b89f79a989e66
Felipe Catão do Nascimento <cataofelipe@gmail.com>
2020-06-15 23:49:23 -0300

    add limits to box

--- engine/js/CharsMove.js
+++ engine/js/CharsMove.js
@@ -2,23 +2,47 @@ var charTarget= document.getElementById("char")
 var speed = 30;
 var rateY=4;
 var rateX=4;
+var limitX_left=3544
+var limitX_rigth=-1376
+var limitY_down=244
+var limitY_up=-1196
 document.addEventListener("keydown",(event)=>{
     const keyname = event.key;
 
     if(event.keyCode==39){
         rateX+=speed
-        charTarget.style.left=rateX+'px'
+        if(rateX<=limitX_left){
+            charTarget.style.left=rateX+'px'
+        }
+        else{
+            rateX-=speed
+        }
     }
     if(event.keyCode==37){
         rateX-=speed
-        charTarget.style.left=rateX+'px'
+        if(rateX>=limitX_rigth){
+            charTarget.style.left=rateX+'px'
+        }
+        else{
+            rateX+=speed
+        }
     }
     if(event.keyCode==40){
         rateY+=speed
-        charTarget.style.top= rateY+'px'
+        if(rateY<=limitY_down){
+            charTarget.style.top= rateY+'px'
+        }
+        else{
+            rateY-=speed
+        }
     }
     if(event.keyCode==38){
         rateY-=speed
-        charTarget.style.top=rateY+'px'
+        if(rateY>=limitY_up){
+            charTarget.style.top=rateY+'px'
+        }
+        else{
+            rateY+=speed
+        }
     }
 })
